24d9cb83128e7170ce50295dce688ddd4e824306
timzeeex-tech <timzeeex@gmail.com>
2026-05-01 01:44:18 +0500

    restore correct Message model

--- models/Message.js
+++ models/Message.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
+
 const messageSchema = new mongoose.Schema({
   chat: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true },
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   text: String,
-  image: String,   // base64 или URL
+  image: String,
+  video: String,
+  audio: String,
+  sticker: String,
   reactions: [{
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     emoji: String,
@@ -11,4 +15,5 @@ const messageSchema = new mongoose.Schema({
   }],
   createdAt: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model('Message', messageSchema);
